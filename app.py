379c32577208c3bbe9f5345d00c00a3c3ae02b78
jpko <tead1234@gmail.com>
2023-06-21 15:08:37 +0900

    add db

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, request
-from databaseService import databaseService
+from databaseService import DatabaseService
 app = Flask(__name__) # 초기화
+## db 
+db = DatabaseService('127.0.0.1',
+            'root',
+            '1234',
+            'cafe_DB',
+            'utf8')
+
+
 @app.route('/') # 요청 주소
 def hello_world(): 
     return 'Hello, World!'
-@app.route('/menu', methods = ['GET', 'POST']) # 요청 주소
+@app.route('/menu', methods = ['GET', 'POST']) # 요청 주소g
 def menu(): 
     return render_template(
         'menu.html'
@@ -13,11 +21,29 @@ def menu():
 def index():
     ## post 
     if request.method == 'POST':
-        form = request.form
         
-    return render_template(
-        'index.html'
-    )
+        form = request.form
+        cafeName = form['cafe-name']
+        sql = '''
+            insert into cafe (id, name,address)
+            values(null, %s, null)
+        '''
+        conn = db.getConn()
+        cursor = db.getCursor()
+        cursor.execute(sql,(cafeName))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        
+        return render_template(
+            'index.html'
+        )
+
+    else:    
+
+        return render_template(
+            'index.html'
+        )
 
 if __name__ == '__main__':
     app.run(debug= True)
--- databaseService.py
+++ databaseService.py
@@ -1,7 +1,13 @@
 import pymysql
 
-class databaseService:
-    def __init__(self, ho, us, pass,d, char):
-        conn = pymysql.connect(host=ho, user= us, password= pass,
-            db= d, charset= char)
-        return conn.cursor()
+
+class DatabaseService:
+    def __init__(self, ho, us, passd, d, char):
+        self.conn = pymysql.connect(host=ho, user=us, password=passd, db=d, charset=char)
+        self.cursor = self.conn.cursor()
+    
+    def getCursor(self):
+        return self.cursor
+    
+    def getConn(self):
+        return self.conn
